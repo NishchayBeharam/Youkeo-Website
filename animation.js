773af6637d531f104e8f0c902574d01b5d79e53e
rhyostudios <108738043+rhyostudios@users.noreply.github.com>
2023-12-12 02:16:02 +0530

    sectionOneComplete

--- animation.js
+++ animation.js
@@ -1,42 +1,100 @@
 // Page Transition
 
+// barba.init({
+//     transitions: [{
+//       name: 'opacity-transition',
+//       leave(data) {
+//         return gsap.to(data.current.container, {
+//           opacity: 0
+//         });
+//       },
+//       enter(data) {
+//         return gsap.from(data.next.container, {
+//           opacity: 0
+//         });
+//       }
+//     }]
+//   });
+
 barba.init({
-    transitions: [{
-      name: 'opacity-transition',
-      leave(data) {
-        return gsap.to(data.current.container, {
-          opacity: 0
-        });
-      },
-      enter(data) {
-        return gsap.from(data.next.container, {
-          opacity: 0
-        });
-      }
+    transition: [{
+        leave(date){
+            gsap.to(data.current.container, {
+                y: '100vw',
+                duration: 2
+            })
+        },
+        enter(data){
+            gsap.from(data.current.container, {
+                y: 0,
+                duration: 2
+            })
+        }
     }]
-  });
+})
+
+// barba.init({
+
+//     sync: true,
+
+//     transition: [{
+//         async leave(data) {
+//             const done = this.async()
+//             leaveTransition()
+//             await delay(1500)
+//             done()
+//         },
+//         async enter(data) {
+//             enterTransition()
+//         }
+//     }]
+// })
+
+//   GSAP Page Transition
+
+const leaveTransition = () => {
+    var t1 = gsap.timeline()
+    t1.to('.transition-overlay',{
+        y: 0,
+        ease: "power1.in"
+    })
+}
+
+const enterTransition = () => {
+    var t1 = gsap.timeline()
+    t1.to('.transition-overlay',{
+        y: '100%',
+        ease: "power1.in"
+    })
+}
 
 // Navbar Click
+
+gsap.to('.dark-overlay',{
+    duration: 1,
+    y: '100%',
+    ease: "power1.in"
+})
 
 const burgerMenu = document.getElementById('burger-menu-wrapper')
 const bmiclinea = document.getElementById('burger-menu-icon-closed-line-a')
 const bmiclineb = document.getElementById('burger-menu-icon-closed-line-b')
 const bmiclinec = document.getElementById('burger-menu-icon-closed-line-c')
+const navLinksPrimary = document.querySelectorAll('.nav-open-left-primary-menu-link')
+const navLinksSecondary = document.querySelectorAll('.nav-open-left-secondary-menu-link')
+const navLogoWrapper = document.querySelector('.nav-closed-logo-wrapper')
 
 let closed = false
 
-burgerMenu.addEventListener('click', async ()=>{
+const navControl = () => {
+
     bmiclinea.classList.toggle('burger-menu-icon-closed-line-a-open')
     bmiclineb.classList.toggle('burger-menu-icon-closed-line-b-open')
     bmiclinec.classList.toggle('burger-menu-icon-closed-line-c-open')
     burgerMenu.classList.toggle('burger-menu-wrapper-open')
+    navLogoWrapper.classList.toggle('nav-closed-logo-wrapper-open')
 
     if (!closed) {
-        gsap.to('.nav-open-right-socials-wrapper',{
-            right: 48,
-            opacity: 1,
-            ease: "power1.out"
-        })
         const navOpen = gsap.timeline().to('.nav-open-left',{
             left: 0,
             duration: 0.8,
@@ -70,16 +128,22 @@ burgerMenu.addEventListener('click', async ()=>{
             opacity: 0
         },'>-0.1').from('.nav-open-left-secondary-menu-links-wrapper',{
             opacity: 0
-        })
+        },'>-0.1').to('.nav-open-right-socials-wrapper',{
+            bottom: 48,
+            opacity: 1,
+            duration: 0.2,
+            ease: "power1.out"
+        },'>-0.3')
         closed=true
     } else {
         gsap.to('.nav-open-left',{
-            left: '-100%',
-            duration: 2,
+            left: '-120%',
+            duration: 0.5,
+            opacity: 0,
             ease: "power1.out"
         })
         gsap.to('.nav-open-right-socials-wrapper',{
-            right: '-100%',
+            bottom: '-48px',
             duration: 2,
             opacity: 0,
             ease: "power1.out"
@@ -87,6 +151,21 @@ burgerMenu.addEventListener('click', async ()=>{
         closed=false
     }
 
-    console.log(closed);
+}
 
+burgerMenu.addEventListener('click', ()=>{
+    navControl()
+})
+
+
+navLinksPrimary.forEach(navLink => {
+    navLink.addEventListener('click', ()=>{
+        navControl()
+    })
+})
+
+navLinksSecondary.forEach(navLink => {
+    navLink.addEventListener('click', ()=>{
+        navControl()
+    })
 })
